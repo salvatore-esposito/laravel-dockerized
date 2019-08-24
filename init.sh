7e58d7ddc1c9bbfb8f03f729d471aeea4e77de3b
vagrant <vagrant@vagrant.vm>
2019-08-24 21:31:20 +0000

    changed php-apache to use php artisan serve

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@
 git clone https://github.com/laravel/laravel.git ./code
 
 docker-compose up --build -d
-docker cp ./code/.env.example app:/var/www/html/.env
-docker cp ./.config/laravel/.htaccess app:/var/www/html/.htaccess
 docker-compose exec app composer install
-docker-compose exec app chown -R www-data: .
+docker cp ./code/.env.example app:/var/www/html/.env
 docker-compose exec app php artisan key:generate
+
+#to start php laravel internal server on all ip numbers digit:
+#docker-compose exec app php artisan serve --host=0.0.0.0
