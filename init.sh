45bef14422d1738af004403cb984b88b5424fa76
Salvatore Esposito <salvatore.esposito78@gmail.com>
2019-09-11 16:09:23 +0200

    fixed some bugs and added browsersync

--- init.sh
+++ init.sh
@@ -2,10 +2,16 @@
 # clone the laravel latest commit to code directory
 git clone https://github.com/laravel/laravel.git ./code
 
+cp ./.config/laravel/.env ./code/.env
+cp ./.config/laravel/webpack.mix.js ./code/webpack.mix.js
+
 docker-compose up --build -d
+
 docker-compose exec app composer install
-docker cp ./.config/laravel/.env app:/var/www/html/.env
 docker-compose exec app php artisan key:generate
 
-#to start php laravel internal server on all ip numbers digit:
-#docker-compose exec app php artisan serve --host=0.0.0.0
+docker-compose exec app npm install
+
+docker-compose exec app php artisan serve &
+docker-compose exec app npm run watch-poll
+docker-compose exec app npm run watch-poll
